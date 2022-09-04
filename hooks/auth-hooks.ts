0a0da0b40a456a507df13e9a224a51c47e7553b0
Pedro Oliveira <pluiz@mychirotouch.com>
2022-09-04 16:47:24 -0300

    add info query

--- hooks/auth-hooks.ts
+++ hooks/auth-hooks.ts
@@ -1,28 +1,48 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import * as AuthApi from '../services/auth-service';
 
 export function useSignInMutation() {
+    const queryClient = useQueryClient();
     return useMutation({
         mutationFn: AuthApi.signIn,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['info']);
+        }
     });
 }
 
 
 export function useSignUpMutation() {
+    const queryClient = useQueryClient();
     return useMutation({
         mutationFn: AuthApi.SignUp,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['info']);
+        }
     });
 }
 
 export function useSignInAdminMutation() {
+    const queryClient = useQueryClient();
     return useMutation({
         mutationFn: AuthApi.signInAdmin,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['info']);
+        }
     });
 }
 
 
 export function useSignUpAdminMutation() {
+    const queryClient = useQueryClient();
     return useMutation({
         mutationFn: AuthApi.SignUpAdmin,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['info']);
+        }
     });
+}
+
+export function useInfo() {
+    return useQuery('info', AuthApi.info);
 }
