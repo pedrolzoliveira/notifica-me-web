f62ae453ff1b2d7c75096f6a1fb8f930bef16990
Pedro Oliveira <pluiz@mychirotouch.com>
2022-10-05 22:46:24 -0300

    refactor(marketplace): better search

--- pages/marketplace/index.tsx
+++ pages/marketplace/index.tsx
@@ -1,27 +1,30 @@
-import { useEffect, useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { AiOutlineLoading3Quarters } from 'react-icons/ai'
+import { Button } from '../../components/Button'
 import { ServicePack } from '../../components/service-pack'
 import { useSearchPlans } from '../../hooks/plans-hooks'
-
-let timeout: NodeJS.Timeout
 
 export const MarketPlace = () => {
   const [searchTextVisual, setSearchTextVisual] = useState('')
   const [searchTextReal, setSearchTextReal] = useState(searchTextVisual)
   const { data: payload, isLoading } = useSearchPlans(searchTextReal)
 
-  useEffect(() => {
-    if (searchTextVisual.length > 3 || searchTextVisual.length === 0) {
-      if (timeout) clearTimeout(timeout)
-      timeout = setTimeout(() => setSearchTextReal(searchTextVisual), 500)
-    }
-  }, [searchTextVisual])
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setSearchTextReal(searchTextVisual)
+  }
 
   return (
     <div className='p-4 w-full space-y-4'>
-      <div>
-        <input className='bg-red-50' type='text' value={searchTextVisual} onChange={e => setSearchTextVisual(e.target.value)}/>
-      </div>
+      <form onSubmit={onSubmit}
+        className='flex items-center space-x-2 w-full'>
+        <input
+          className='w-full rounded px-4 py-2 border bg-gray-50'
+          type='text'
+          value={searchTextVisual}
+          onChange={e => setSearchTextVisual(e.target.value)}/>
+        <Button>Pesquisar</Button>
+      </form>
       {
         isLoading
           ? <AiOutlineLoading3Quarters className='animate-spin'/>
